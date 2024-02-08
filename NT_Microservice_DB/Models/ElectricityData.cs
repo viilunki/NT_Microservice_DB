c8e269e34eedad150409873d242c16e950f345c2
Ville Ellilä <dviel001@edu.xamk.fi>
2024-02-08 09:45:33 +0200

    ElectricityData class updated

--- NT_Microservice_DB/Models/ElectricityData.cs
+++ NT_Microservice_DB/Models/ElectricityData.cs
@@ -2,9 +2,20 @@
 
 namespace NT_Microservice_DB.Models
 {
-    [Keyless]
+    using Microsoft.EntityFrameworkCore;
+
+    public class MyDbContext : DbContext
+    {
+        public DbSet<ElectricityData> ElectricityDatas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
     public class ElectricityData
     {
+        public int Id { get; set; }
         public decimal price { get; set; }
         public DateTime startDate { get; set; }
         public DateTime endDate { get; set; }
